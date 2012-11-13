bbd6e3170b50086b483c8c1e54ef9a8e7f343645
dhacker29 <davidhackerdvm@gmail.com>
2012-11-13 01:34:40 -0600

    Create vendor/motorola/msm8960-common

--- vanquish-vendor-blobs.mk
+++ vanquish-vendor-blobs.mk
@@ -270,10 +270,3 @@ PRODUCT_COPY_FILES += \
 
 #PRODUCT_PROPERTY_OVERRIDES += \
 #    drm.service.enabled=true
-
-# Proprietary VZW blobs for LTE
-
-PRODUCT_PACKAGES += \
-	VZWAPNLib \
-	vzwapnpermission \
-	VZWAPNService
